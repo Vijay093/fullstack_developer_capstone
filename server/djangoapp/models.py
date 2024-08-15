7075777afacb8968489f20391a11fb2c7a10cd82
Vijay093 <Vijay.rathnappa42@gmail.com>
2024-08-15 03:57:42 -0400

    Code beautified for linting purpose

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -11,7 +11,7 @@ class CarMake(models.Model):
     description = models.TextField()
 
     def __str_(self):
-        return self.name 
+        return self.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -29,11 +29,13 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SEDAN')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
 
     def __str__(self):
         return self.name
